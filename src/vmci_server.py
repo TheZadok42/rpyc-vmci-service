2402646b01b58ec0491b55179ae7d8580322cd74
Tal Tzadok <zadok42@protonmail.com>
2022-04-15 13:00:33 +0300

    Fixed host for vsock

--- src/vmci_server.py
+++ src/vmci_server.py
@@ -1,8 +1,20 @@
+import fcntl
 import socket
+import struct
 
 from rpyc.utils.server import ThreadedServer
 
 ANY_CID = -1
+
+VSOCK_DEVICE_PATH = '/dev/vsock'
+
+
+def get_local_cid():
+    with open(VSOCK_DEVICE_PATH, 'rb') as vsock_device:
+        raw_result = fcntl.ioctl(vsock_device,
+                                 socket.IOCTL_VM_SOCKETS_GET_LOCAL_CID,
+                                 ' ' * 4)
+    return struct.unpack('<L', raw_result)[0]
 
 
 class VMCISocketServer(ThreadedServer):
@@ -14,5 +26,7 @@ class VMCISocketServer(ThreadedServer):
         # Creating a new listener using VSOCK as the protocol
         self.listener = socket.socket(socket.AF_VSOCK, socket.SOCK_STREAM)
         self.listener.bind((ANY_CID, port))
-        sock_details = self.listener.getsockname()
-        self.host, self.port = sock_details[0], sock_details[1]
+        # The original ThreadedServer uses getsockname to get the host and the port
+        # This doesn't return the right host for VSOCK
+        self.host = get_local_cid()
+        self.port = port
