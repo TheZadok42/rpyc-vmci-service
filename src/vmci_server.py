42a9adbb10055e74a4805ef8571178c55dd31f40
Tal Tzadok <zadok42@protonmail.com>
2022-04-15 13:08:06 +0300

    orgeneized

--- src/vmci_server.py
+++ src/vmci_server.py
@@ -4,17 +4,23 @@ import struct
 
 from rpyc.utils.server import ThreadedServer
 
-ANY_CID = -1
+from .consts import ANY_CID, CID_STRUCT_FORMAT, UNIX_VSOCK_DEVICE_PATH
 
-VSOCK_DEVICE_PATH = '/dev/vsock'
+
+def _get_local_vsock_device_path():
+    # TODO: add windows support
+    return UNIX_VSOCK_DEVICE_PATH
 
 
 def get_local_cid():
-    with open(VSOCK_DEVICE_PATH, 'rb') as vsock_device:
+    local_vsock_device_path = _get_local_vsock_device_path()
+    buffer_string = ' ' * 4
+    with open(local_vsock_device_path, 'rb') as vsock_device:
         raw_result = fcntl.ioctl(vsock_device,
                                  socket.IOCTL_VM_SOCKETS_GET_LOCAL_CID,
-                                 ' ' * 4)
-    return struct.unpack('<L', raw_result)[0]
+                                 buffer_string)
+    # struct alaways returns a tuple, we only need the first number
+    return struct.unpack(CID_STRUCT_FORMAT, raw_result)[0]
 
 
 class VMCISocketServer(ThreadedServer):
@@ -24,9 +30,11 @@ class VMCISocketServer(ThreadedServer):
         # Closing the default listener created by threaded server
         self.listener.close()
         # Creating a new listener using VSOCK as the protocol
+        # TODO: add windows support
         self.listener = socket.socket(socket.AF_VSOCK, socket.SOCK_STREAM)
         self.listener.bind((ANY_CID, port))
-        # The original ThreadedServer uses getsockname to get the host and the port
-        # This doesn't return the right host for VSOCK
+        # The original ThreadedServer uses getsockname
+        # to get the host and the port
+        # This doesn't return the right host for VSOCK, so we get the local cid
         self.host = get_local_cid()
         self.port = port
